28f7d16d03cdd457af0eade745c05a1ae23b70aa
Willoughby Caine <155117794+wcaine93@users.noreply.github.com>
2024-07-08 18:06:16 +0000

    Feature: Created GUI struct, cleaned up lifetimes

--- src/engine.rs
+++ src/engine.rs
@@ -16,40 +16,34 @@ pub trait Update {
     fn update(&mut self, delta: f64, world: &mut World, id: i64) {}
 }
 
-struct Entity<'a> {
-    data: Box<dyn Update + 'a>,
+struct Entity {
+    data: Box<dyn Update>,
     id: i64,
 }
 
-impl<'a> Update for Entity<'a> {
+impl<'a> Update for Entity {
     fn update(&mut self, delta: f64, world: &mut World, id: i64) {
         self.data.update(delta, world, id);
     }
 }
 
-pub struct World<'a> {
-    entities: Vec<Entity<'a>>,
-    last_id: i64,
-    stdout: Stdout,
+pub struct World {
+    entities: Vec<Entity>,
     map: Vec<Vec<(char, Color)>>,
-    pub current_input: Option<KeyCode>,
 }
 
-impl<'a> World<'a> {
+impl<'a> World {
     pub fn new(map_width: usize, map_height: usize) -> Self {
         World {
             entities: Vec::new(),
-            last_id: 0,
-            current_input: None,
-            stdout: io::stdout(),
             map: vec![vec![(' ', Color::Black); map_height]; map_width],
         }
     }
 
-    pub fn add_entity<T: Update + 'a>(&'_ mut self, entity: T) {
+    pub fn add_entity(&'_ mut self, entity: impl Update + 'static) {
         self.entities.push(Entity {
             data: Box::new(entity),
-            id: self.last_id,
+            id: self.entities.len() as i64 + 1,
         })
     }
 
@@ -74,27 +68,10 @@ impl<'a> World<'a> {
         self.map[position.0 as usize][position.1 as usize] = (character, color);
     }
 
-    fn terminal_draw(
-        &mut self,
-        character: char,
-        position: (u16, u16),
-        color: Color,
-    ) -> io::Result<()> {
-        self.stdout
-            .queue(cursor::MoveTo(position.0, position.1))?
-            .queue(style::PrintStyledContent((character).with(color)))?;
-        Ok(())
-    }
-    pub fn debug_draw(&mut self, text: &str) -> io::Result<()> {
-        self.stdout
-            .queue(cursor::MoveTo(0, self.map[0].len() as u16))?
-            .queue(style::PrintStyledContent((text).with(Color::Red)))?;
-        Ok(())
-    }
     fn draw_map(&mut self) {
         for r in 0..self.map.len() {
             for c in 0..self.map[0].len() {
-                let _ = self.terminal_draw(
+                let _ = GUI::terminal_draw(
                     self.map[r][c].0,
                     (r as u16, c as u16),
                     self.map[r][c].1,
@@ -111,11 +88,11 @@ impl<'a> World<'a> {
         }
     }
 
-    pub fn init(&mut self) -> io::Result<()> {
+    pub fn init(&mut self, gui: GUI) -> io::Result<()> {
         let _ = terminal::enable_raw_mode();
         let mut now = SystemTime::now();
 
-        self.stdout
+        gui.stdout
             .execute(terminal::Clear(terminal::ClearType::All))?
             .execute(Hide)?;
 
@@ -130,22 +107,22 @@ impl<'a> World<'a> {
                 Ok(elapsed) => {
                     now = SystemTime::now();
 
-                    self.current_input = match rx.try_recv() {
+                    gui.current_input = match rx.try_recv() {
                         Ok(ko) => match ko {
                             Some(k) => Some(k),
-                            None => self.current_input,
+                            None => gui.current_input,
                         },
-                        Err(_) => self.current_input,
+                        Err(_) => gui.current_input,
                     };
 
-                    if self
+                    if gui
                         .current_input
                         .is_some_and(|x| x == KeyCode::Char('q'))
                     {
                         break;
                     }
 
-                    self.update(elapsed.as_secs_f64());
+                    self.update(gui, elapsed.as_secs_f64());
                 }
                 Err(e) => {
                     println!("Error: {e:?}");
@@ -153,7 +130,7 @@ impl<'a> World<'a> {
             }
         }
 
-        self.stdout
+        gui.stdout
             .execute(terminal::Clear(terminal::ClearType::All))?
             .execute(MoveTo(0, 0))?
             .execute(Show)?;
@@ -162,7 +139,7 @@ impl<'a> World<'a> {
         Ok(())
     }
 
-    fn update(&mut self, delta: f64) {
+    fn update(&mut self, gui: GUI, delta: f64) {
         let mut entity_queue: Vec<Entity> = Vec::new();
         entity_queue.append(&mut self.entities);
         for entity in entity_queue.iter_mut() {
@@ -171,7 +148,7 @@ impl<'a> World<'a> {
         self.entities.append(&mut entity_queue);
 
         self.draw_map();
-        _ = self.stdout.flush();
+        _ = gui.stdout.flush();
         self.clear_map();
     }
 }
@@ -183,5 +160,32 @@ fn read_inputs() -> Option<KeyCode> {
             _ => None,
         },
         _ => None,
+    }
+}
+
+pub struct GUI {
+    stdout: Stdout,
+    pub current_input: Option<KeyCode>,
+}
+
+impl Default for GUI {
+    fn default() -> Self {
+        GUI {
+            current_input: None,
+            stdout: io::stdout(),
+        }
+    }
+}
+
+impl GUI {
+    pub fn terminal_draw(
+        character: char,
+        position: (u16, u16),
+        color: Color,
+    ) -> io::Result<()> {
+        io::stdout()
+            .queue(cursor::MoveTo(position.0, position.1))?
+            .queue(style::PrintStyledContent((character).with(color)))?;
+        Ok(())
     }
 }
